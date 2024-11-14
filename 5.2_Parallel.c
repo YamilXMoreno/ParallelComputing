fd1403c68ddaed7b0ebcb644aedd0e062a12d881
Yamil J Moreno <148034405+YamilXMoreno@users.noreply.github.com>
2024-11-13 23:48:31 -0600

    Update 5.2_Parallel.c

--- 5.2_Parallel.c
+++ 5.2_Parallel.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>  // Include time.h for the time function
 #include <omp.h>
 
-double monte_carlo_pi(long long num_tosses) {
-    long long number_in_circle = 0;
+double monte_carlo_pi(long long int number_of_tosses) {
+    long long int number_in_circle = 0;
 
     #pragma omp parallel reduction(+:number_in_circle)
     {
-        unsigned int seed = (unsigned int) time(NULL) ^ omp_get_thread_num();
+        unsigned int seed = (unsigned int) time(NULL) ^ omp_get_thread_num(); // Unique seed for each thread
+
         #pragma omp for
-        for (long long toss = 0; toss < num_tosses; toss++) {
-            double x = (rand_r(&seed) / (double) RAND_MAX) * 2.0 - 1.0;
-            double y = (rand_r(&seed) / (double) RAND_MAX) * 2.0 - 1.0;
-            if (x * x + y * y <= 1) {
+        for (long long int toss = 0; toss < number_of_tosses; toss++) {
+            double x = (double)rand_r(&seed) / RAND_MAX * 2.0 - 1.0;  // Random x between -1 and 1
+            double y = (double)rand_r(&seed) / RAND_MAX * 2.0 - 1.0;  // Random y between -1 and 1
+            double distance_squared = x * x + y * y;
+
+            if (distance_squared <= 1) {
                 number_in_circle++;
             }
         }
     }
 
-    return 4.0 * number_in_circle / num_tosses;
+    return 4.0 * number_in_circle / ((double) number_of_tosses);
 }
 
 int main() {
-    long long num_tosses = 1000000;
-    double pi_estimate = monte_carlo_pi(num_tosses);
+    long long int number_of_tosses;
+    printf("Enter the number of tosses: ");
+    scanf("%lld", &number_of_tosses);
+
+    double pi_estimate = monte_carlo_pi(number_of_tosses);
     printf("Estimated Pi = %f\n", pi_estimate);
+
     return 0;
 }
