e4192939dfd771ee1a4cb24e1d4184aa23f15df8
Yamil J Moreno <148034405+YamilXMoreno@users.noreply.github.com>
2024-11-13 23:44:40 -0600

    Update 5.1_Parallel.c

--- 5.1_Parallel.c
+++ 5.1_Parallel.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>  // Include stdlib.h for calloc and free
 #include <omp.h>
 
 void parallel_histogram(int *data, int data_size, int *histogram, int num_bins) {
     int i;
+
     // Initialize global histogram to zero
-    for (i = 0; i < num_bins; i++) histogram[i] = 0;
+    for (i = 0; i < num_bins; i++) {
+        histogram[i] = 0;
+    }
 
     #pragma omp parallel
     {
-        int *local_histogram = calloc(num_bins, sizeof(int)); // Thread-local histogram
+        int *local_histogram = (int *)calloc(num_bins, sizeof(int)); // Thread-local histogram
 
         // Each thread processes a subset of data
         #pragma omp for
@@ -23,6 +27,23 @@ void parallel_histogram(int *data, int data_size, int *histogram, int num_bins) 
             histogram[i] += local_histogram[i];
         }
 
-        free(local_histogram);
+        free(local_histogram); // Free allocated memory for each thread's local histogram
     }
 }
+
+int main() {
+    int data[] = {1, 2, 2, 3, 4, 5, 5, 6, 7, 8, 8, 9};  // Example data
+    int data_size = sizeof(data) / sizeof(data[0]);
+    int num_bins = 10;  // Set number of bins
+    int histogram[num_bins];
+
+    parallel_histogram(data, data_size, histogram, num_bins);
+
+    // Display the histogram
+    for (int i = 0; i < num_bins; i++) {
+        printf("Bin %d: %d\n", i, histogram[i]);
+    }
+
+    return 0;
+}
+
